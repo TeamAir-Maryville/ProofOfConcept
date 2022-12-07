a5f67eff9cdf20f6503e833fa1b48e709c5f057f
codeklor <118039966+codeklor@users.noreply.github.com>
2022-12-07 11:53:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,204 +1,124 @@
-'''
-Created on Nov 27, 2022
-Last Updated on Dec 4, 2022
- 
-@author: Alison Glore
-
-READ ME
-Kivy Framework
-
-'''
-
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
+import random
 
-Builder.load_string("""
-<MenuScreen>:
-    canvas.before:
-        Color: 
-            rgba: (1, 222/255, 167/255, 1) 
-        Rectangle: 
-            pos: self.pos
-            size: self.size
 
-    BoxLayout:
-        orientation: 'vertical'
-        Label: 
-            text: 'WATCHER'
-            font_size: '60sp'
-            size_hint: 1,1
-            color: (0, 0, 0, 1)
-        BoxLayout: 
-            orientation: 'horizontal'
-            Button:
-                text: 'Add Show'
-                font_size: '40sp'
-                size_hint: .5, 1
-                pos_hint: { 'left': .5}
-                background_normal: ''
-                background_color: (1, 199/255, 146/255, 1)
-                color: (0, 0, 0, 1)
-                on_press: root.manager.current = 'Add Show'
-            Button: 
-                text: 'Add Movie'
-                font_size: '40sp'
-                size_hint: .5, 1
-                pos_hint: { 'right': .5}
-                background_normal: ''
-                background_color: (1, 199/255, 146/255, 1)
-                color: (0, 0, 0, 1)
-                on_press: root.manager.current = 'Add Movie'
-        Button:
-            text: 'Watch Now'
-            font_size: '40sp'
-            size_hint: .75, 1
-            pos_hint: { 'center_x': .5}
-            background_normal: ''
-            background_color: (252/255, 149/255, 45/255, 1)
-            color: (0, 0, 0, 1)
-            on_press: root.manager.current = 'Watch Now'
-
-<AddShowScreen>:
-    canvas.before:
-        Color: 
-            rgba: (1, 222/255, 167/255, 1) 
-        Rectangle: 
-            pos: self.pos
-            size: self.size
-
-    BoxLayout:
-        orientation: 'vertical'
-        Button:
-            text: 'Back to Menu'
-            font_size: '40sp'
-            size_hint: .5, 1
-            pos_hint: { 'left': .5}
-            background_normal: ''
-            background_color: (252/255, 149/255, 45/255, 1)
-            color: (0, 0, 0, 1)
-            on_press: root.manager.current = 'Menu'
-        Label:
-            text: 'SHOWS'
-            font_size: '60sp'
-            size_hint: 1,1
-            color: (0, 0, 0, 1)
-        BoxLayout: 
-            orientation: 'vertical'
-            BoxLayout: 
-                orientaion: 'horizontal'
-                Button:
-                    text: 'Insert Title'
-                    font_size: '40sp'
-                    size_hint: .5, 1
-                    pos_hint: { 'center_x': .5}
-                    background_normal: ''
-                    background_color: (1, 199/255, 146/255, 1)
-                    color: (0, 0, 0, 1)
-                Button:
-                    text: 'Insert Watch Time'
-                    font_size: '40sp'
-                    size_hint: .5, 1
-                    pos_hint: { 'center_x': .5}
-                    background_normal: ''
-                    background_color: (1, 199/255, 146/255, 1)
-                    color: (0, 0, 0, 1)
-        Button:
-            text: 'Enter'
-            font_size: '40sp'
-            size_hint: .75, 1
-            pos_hint: { 'center_x': .5}
-            background_normal: ''
-            background_color: (252/255, 149/255, 45/255, 1)
-            color: (0, 0, 0, 1)
-
-<AddMovieScreen>:
-    canvas.before:
-        Color: 
-            rgba: (1, 222/255, 167/255, 1) 
-        Rectangle: 
-            pos: self.pos
-            size: self.size
-
-    BoxLayout:
-        orientation: 'vertical'
-        Button:
-            text: 'Back to Menu'
-            font_size: '40sp'
-            size_hint: .5, 1
-            pos_hint: { 'left': .5}
-            background_normal: ''
-            background_color: (252/255, 149/255, 45/255, 1)
-            color: (0, 0, 0, 1)
-            on_press: root.manager.current = 'Menu'
-        Label:
-            text: 'MOVIES'
-            font_size: '60sp'
-            size_hint: 1,1
-            color: (0, 0, 0, 1)
-        BoxLayout: 
-            orientation: 'vertical'
-            BoxLayout: 
-                orientaion: 'horizontal'
-                Button:
-                    text: 'Insert Title'
-                    font_size: '40sp'
-                    size_hint: .5, 1
-                    pos_hint: { 'center_x': .5}
-                    background_normal: ''
-                    background_color: (1, 199/255, 146/255, 1)
-                    color: (0, 0, 0, 1)
-                Button:
-                    text: 'Insert Watch Time'
-                    font_size: '40sp'
-                    size_hint: .5, 1
-                    pos_hint: { 'center_x': .5}
-                    background_normal: ''
-                    background_color: (1, 199/255, 146/255, 1)
-                    color: (0, 0, 0, 1)
-        Button:
-            text: 'Enter'
-            font_size: '40sp'
-            size_hint: .75, 1
-            pos_hint: { 'center_x': .5}
-            background_normal: ''
-            background_color: (252/255, 149/255, 45/255, 1)
-            color: (0, 0, 0, 1)
-
-""")
-
-class MenuScreen(Screen):
+class HomeScreen(Screen):
     pass
 
-class AddShowScreen(Screen):
-    def addShow(self, title, duration):
-        with open("shows_list.txt", "a") as showList:
-            showList.write(title.text + "," + duration.text + "\n")
-        title.text = ''
-        duration.text = ''
-    pass
 
 class AddMovieScreen(Screen):
     def addMovie(self, title, duration):
-        with open("movies_list.txt", "a") as showList:
-            showList.write(title.text + "," + duration.text + "\n")
+        with open("movies_list.txt", "a") as showLst:
+            showLst.write(title.text + "," + duration.text + "\n")
         title.text = ''
         duration.text = ''
+
     pass
 
-class TestApp(App):
 
+class AddTvScreen(Screen):
+
+    def addShow(self, title, duration):
+        with open("shows_list.txt", "a") as showLst:
+            showLst.write(title.text + "," + duration.text + "\n")
+        title.text = ''
+        duration.text = ''
+
+    pass
+
+
+class GetRecommendationScreen(Screen):
+    pass
+
+
+class GetRecommendationMovieScreen(Screen):
+    def movieRecommendation(self, availableTime):
+        if availableTime is None:
+            exit()
+        movies = []
+        with open("movies_list.txt", "r") as movieLst:
+            lines = movieLst.readlines()
+            for line in lines:
+                line = line.strip()
+                movies.append(line)
+
+        while True:
+            index = random.randrange(0, len(movies))
+            recommendation = movies[index]
+            recommendation = recommendation.split(",")
+
+            if int(recommendation[1]) <= int(availableTime.text):
+                recommendationName = recommendation[0]
+
+                layout = GridLayout(cols=1, padding=10)
+                popupLabel = Label(text=str(recommendationName))
+                closeButton = Button(text="Close the pop-up")
+                layout.add_widget(popupLabel)
+                layout.add_widget(closeButton)
+                popup = Popup(title='Recommendation',
+                              size_hint=(None, None),
+                              size=(400, 400),
+                              content=layout)
+                popup.open()
+                closeButton.bind(on_press=popup.dismiss)
+                break
+
+    pass
+
+
+class GetRecommendationTvScreen(Screen):
+    def tvShowRecommendation(self, availableTime):
+        if availableTime is None:
+            exit()
+        movies = []
+        with open("shows_list.txt", "r") as movieLst:
+            lines = movieLst.readlines()
+            for line in lines:
+                line = line.strip()
+                movies.append(line)
+
+        while True:
+            index = random.randrange(0, len(movies))
+            recommendation = movies[index]
+            recommendation = recommendation.split(",")
+
+            if int(recommendation[1]) <= int(availableTime.text):
+                recommendationName = recommendation[0]
+
+                layout = GridLayout(cols=1, padding=10)
+                popupLabel = Label(text=str(recommendationName))
+                closeButton = Button(text="Close the pop-up")
+                layout.add_widget(popupLabel)
+                layout.add_widget(closeButton)
+                popup = Popup(title='Recommendation',
+                              size_hint=(None, None),
+                              size=(400, 400),
+                              content=layout)
+                popup.open()
+                closeButton.bind(on_press=popup.dismiss)
+                break
+
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("settings.kv")
+
+
+class WatcherApp(App):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='Menu'))
-        sm.add_widget(AddShowScreen(name='Add Show'))
-        sm.add_widget(AddMovieScreen(name='Add Movie'))
+        return kv
 
-        return sm
 
 if __name__ == '__main__':
-    TestApp().run()
+    WatcherApp().run()
